881f50767ca8193140295c3580725e52ccedbd8d
beastroza-art <be.astroza@duocuc.cl>
2026-06-15 09:27:15 -0400

    feat: create navbar component

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+
+      <h1>Landing Electricista</h1>
+    </main>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>ElectricPro</h2>
+    </nav>
+  );
+}
